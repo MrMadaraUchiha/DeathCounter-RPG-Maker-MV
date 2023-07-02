69c7e24c3c957e5252d88c3552b3ed2b597ef9fe
Thales Maggot <122075677+MrMadaraUchiha@users.noreply.github.com>
2023-07-02 11:12:15 -0300

    Add files via upload

---
+++ DeathCounter.js
@@ -0,0 +1,34 @@
+//=============================================================================
+// Death Counter Plugin
+// by: MrMadara - https://github.com/MrMadaraUchiha
+//=============================================================================
+
+(function() {
+  // Cria um objeto para o contador de mortes
+  var DeathCounter = {
+    count: 0,
+
+    increment: function() {
+      this.count++;
+    },
+
+    getCount: function() {
+      return this.count;
+    }
+  };
+
+  // Expor o objeto DeathCounter para uso externo
+  window.DeathCounter = DeathCounter;
+
+  //=============================================================================
+  // Game_Interpreter
+  //=============================================================================
+
+  var _Game_Interpreter_pluginCommand = Game_Interpreter.prototype.pluginCommand;
+  Game_Interpreter.prototype.pluginCommand = function(command, args) {
+    _Game_Interpreter_pluginCommand.call(this, command, args);
+    if (command === 'IncrementDeathCounter') {
+      DeathCounter.increment();
+    }
+  };
+})();
