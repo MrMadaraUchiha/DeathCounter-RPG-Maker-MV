9a4c3e180d0382d3150424cec2d60019eb216053
Thales Maggot <122075677+MrMadaraUchiha@users.noreply.github.com>
2023-07-02 11:22:13 -0300

    Update DeathCounter.js
    Nesta versão atualizada, incorporamos as sugestões 1 e 3 ao projeto. Agora, o contador de mortes possui um objeto MilestoneRewards, onde você pode definir os marcos de mortes desejados e as recompensas correspondentes. No exemplo fornecido, temos dois marcos: 10 mortes e 25 mortes.
    
    Além disso, adicionamos a funcionalidade de modos de dificuldade, permitindo ajustar o número máximo de mortes permitidas e a penalidade associada a cada modo.
    
    Você pode usar os comandos de plugin IncrementDeathCounter para incrementar o contador de mortes e SetDeathCounterMode para alterar o modo de dificuldade durante o jogo.
    
    Certifique-se de ajustar as recompensas, marcos de mortes, modos de dificuldade e outras configurações de acordo com suas necessidades específicas.

--- DeathCounter.js
+++ DeathCounter.js
@@ -1,15 +1,67 @@
 //=============================================================================
 // Death Counter Plugin
-// by: MrMadara - https://github.com/MrMadaraUchiha/DeathCounter-RPG-Maker-MV
 //=============================================================================
 
 (function() {
+  // Configurações de Modo de Dificuldade
+  var DifficultySettings = {
+    easy: {
+      maxDeaths: 10, // Número máximo de mortes permitidas no modo fácil
+      penalty: 0 // Penalidade por morte no modo fácil (0 significa nenhuma penalidade)
+    },
+    normal: {
+      maxDeaths: 5, // Número máximo de mortes permitidas no modo normal
+      penalty: 1 // Penalidade por morte no modo normal (1 significa penalidade padrão)
+    },
+    hard: {
+      maxDeaths: 3, // Número máximo de mortes permitidas no modo difícil
+      penalty: 2 // Penalidade por morte no modo difícil (2 significa penalidade dupla)
+    }
+  };
+
+  // Configurações de Recompensa por Marco de Mortes
+  var MilestoneRewards = {
+    10: { item: 1, amount: 3 }, // Exemplo de recompensa no marco de 10 mortes
+    25: { skill: 2 } // Exemplo de recompensa no marco de 25 mortes
+    // Adicione mais marcos de mortes e recompensas conforme necessário
+  };
+
   // Cria um objeto para o contador de mortes
   var DeathCounter = {
     count: 0,
+    mode: "normal", // Modo de dificuldade padrão
+    difficulty: DifficultySettings["normal"],
+
+    setMode: function(mode) {
+      if (DifficultySettings.hasOwnProperty(mode)) {
+        this.mode = mode;
+        this.difficulty = DifficultySettings[mode];
+      }
+    },
 
     increment: function() {
       this.count++;
+      if (this.count > this.difficulty.maxDeaths) {
+        // Aplica a penalidade por morte
+        // (por exemplo, diminuir a saúde do jogador, remover habilidades, etc.)
+        // Aqui, vamos apenas imprimir uma mensagem de penalidade no console.
+        console.log("Você sofreu uma penalidade por morte!");
+      }
+      this.checkMilestoneRewards();
+    },
+
+    checkMilestoneRewards: function() {
+      var rewards = MilestoneRewards;
+      for (var milestone in rewards) {
+        if (this.count >= milestone) {
+          var reward = rewards[milestone];
+          if (reward.item) {
+            $gameParty.gainItem($dataItems[reward.item], reward.amount || 1);
+          } else if (reward.skill) {
+            $gameActors.actor(1).learnSkill(reward.skill);
+          }
+        }
+      }
     },
 
     getCount: function() {
@@ -29,6 +81,10 @@
     _Game_Interpreter_pluginCommand.call(this, command, args);
     if (command === 'IncrementDeathCounter') {
       DeathCounter.increment();
+    }
+    else if (command === 'SetDeathCounterMode') {
+      var mode = args[0].toLowerCase();
+      DeathCounter.setMode(mode);
     }
   };
 })();
